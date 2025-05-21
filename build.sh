9cf236eae63b09b8bce7eaee7b5fd624f365a828
Abino Poulose <abinopoulose@outlook.com>
2025-05-21 15:47:44 +0530

    release @ 2025-05-21 15:47:44

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e  # Exit on any error
+
+#clean 
+git checkout release
+rm -rf assets/ index.html vite.svg || true
+
+#build
+cd digi-sign
+git checkout main
+git pull
+
+npm install
+npm run build
+cd ..
+
+#copy build files correct path
+cp -r digi-sign/dist/* .
+sed -i 's|assets|digi-sign/assets|g' index.html assets/*
+
+
+#commit and push
+git add .
+DATETIME=$(date +"%Y-%m-%d %H:%M:%S")
+git commit -m "release @ $DATETIME"
+git push
+
+
+echo "script completed successfully!"
+
